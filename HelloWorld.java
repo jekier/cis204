7cbd89361c036462c036105e8f6f09544afdca78
Jacob Kier <jkier@jccc.edu>
2022-11-03 13:06:58 -0500

    Removed curly quotes, slight reword.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
+// A simple hello world program
+
 public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println(“Hello,World”);
+        System.out.println("Hello World!");
     }
 }
